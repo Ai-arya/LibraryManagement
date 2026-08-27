af7e88993249662b1b57f553c0b9f2c0061aaa5a
Aryan Jangid <Aryan@gmail.com>
2026-08-27 11:48:14 +0530

    Dashboard

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LibraryManagementSystem.Controllers
+{
+    public class StudentController : Controller
+    {
+        // Student
+        public ActionResult StudentList()
+        {
+            return View();
+        }
+        // add student
+        public ActionResult AddStudent()
+        {
+            return View();
+        }
+        // student details
+        public ActionResult StudentDetails()
+        {
+            return View();
+        }
+    }
+
+}
